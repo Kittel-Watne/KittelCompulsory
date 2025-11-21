6a2064cece9faefb596715b033fb10651a07b6a1
Kittel <142875697+Kittel-Watne@users.noreply.github.com>
2025-11-21 10:51:46 +0100

    fixed issue with numbers out of scope
    There is 1 number in my set which is multiple logarithmical levels larger than the rest, it is now not considered for the minimum/maximum value.
    
    Also made approximations for missing y values in the quads.

--- Triangulering.h
+++ Triangulering.h
@@ -16,12 +16,16 @@ void triangulate(std::vector<Vertex> inVertices){
 
     for (Vertex v : inVertices) {
         if (v.x > maxX)
-            maxX = v.x;
+            if (v.x < maxX + 5000)
+                maxX = v.x;
         if (v.x < minX)
-            minX = v.x;
+            if (v.x > minX - 5000)
+                minX = v.x;
         if (v.z > maxZ)
+            if (v.z < maxZ + 5000)
             maxZ = v.z;
         if (v.z < minZ)
+            if (v.z > minZ - 5000)
             minZ = v.z;
     }
     int amountOfQuadsX = 10;
@@ -33,32 +37,80 @@ void triangulate(std::vector<Vertex> inVertices){
     std::vector<Vertex> quads;
     for (int i = 0; i < amountOfQuadsZ; i++){
         for (int j = 0; j < amountOfQuadsX; j++){
-            quads.push_back(Vertex((j + 0.5f) * quadLength, 0, (i + 0.5f) * quadWidth, 0, 1, 0, 0, 0));
+            quads.push_back(Vertex((j + 0.5f) * quadLength, 0.f, (i + 0.5f) * quadWidth, 0, 1, 0, 0, 0));
         }
     }
 
     for (Vertex v : inVertices){
         float vPositionX;
         float vPositionZ;
-        if (v.x >= ((amountOfQuadsX-1) * quadLength) + minX){
+
+        //If the position is maximum or greater, it is forced to maximum
+        if (v.x >= maxX){
             vPositionX = (amountOfQuadsX-1);
         }
         else{
         vPositionX = floor((v.x - minX)/quadLength);
         }
-        if (v.z >= (amountOfQuadsZ-1) * quadWidth + minZ){
+        //If the position is maximum or greater, it is forced to maximum
+        if (v.z >= maxZ - 1){
             vPositionZ = (amountOfQuadsZ-1);
         }
         else{
             vPositionZ = floor((v.z - minZ)/quadWidth);
         }
-        quads.at(vPositionZ * amountOfQuadsX + vPositionX).y = (quads.at(vPositionZ * amountOfQuadsX + vPositionX).y + v.y) / 2;
+
+        if (vPositionX >= 0 && vPositionX <= 9 && vPositionZ >= 0 && vPositionZ <= 9){
+        //temporarily using the color of the vertex to count how many points are at each quad
+        quads.at(vPositionZ * amountOfQuadsX + vPositionX).r++;
+        quads.at(vPositionZ * amountOfQuadsX + vPositionX).y = (quads.at(vPositionZ * amountOfQuadsX + vPositionX).y + v.y);
+        }
     }
+    //filling in incomplete information
+    for (int i = 0; i< quads.size(); i++)
+    {
+        if (quads.at(i).r == 0){
+            float neighborSum = 0.f;
+            int sums = 0;
+            if (i > 0){
+                neighborSum += quads.at(i - 1).y;
+                sums++;
+            }
+            if (i >= amountOfQuadsX){
+                neighborSum += quads.at(i - amountOfQuadsX).y;
+                sums++;
+            }
+            if (i < quads.size() - 1){
+                neighborSum += quads.at(i + 1).y;
+                sums++;
+            }
+            if (i < quads.size() - amountOfQuadsX){
+                neighborSum += quads.at(i + amountOfQuadsX).y;
+                sums++;
+            }
+            //Finding the average of the neighbor-sums
+            if (sums > 0){
+                quads.at(i).y = neighborSum / sums;
+            }
+            else{
+                quads.at(i).y = 1;
+            }
+        }
+    }
+    for (Vertex q : quads){
+        if (q.r != 0){
+        q.y = q.y / q.r;
+        q.r = 0.f;
+        std::cout << q.x << " " << q.y << " " << q.z << "\n";
+        }
+    }
+
     std::vector<Vertex> vertices;
     //This is incomplete and deletes the outermost part of the model
     for (int i = 0; i < amountOfQuadsZ; i++){
         for (int j = 0; j < amountOfQuadsX; j++){
             vertices.push_back(Vertex(j * quadLength, quads.at(i * amountOfQuadsX + j).y, i * quadWidth, 0, 1, 0, 0, 0));
+            //std::cout << vertices.back() << "\n";
         }
     }
 

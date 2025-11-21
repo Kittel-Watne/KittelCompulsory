c848315cb2504720fb220ae5a11da3aaeb72de09
Kittel <142875697+Kittel-Watne@users.noreply.github.com>
2025-11-21 19:38:22 +0100

    Made approximation for missing values
    Also tested the code for larger quad areas.

--- Triangulering.h
+++ Triangulering.h
@@ -6,7 +6,7 @@
 #include <math.h>
 
 
-void triangulate(std::vector<Vertex> inVertices){
+std::vector<Vertex> triangulate(std::vector<Vertex> inVertices){
     //std::vector<float> verticesCopy = inVertices;
     float maxX = -1.f;
     float minX = 1000.0f;
@@ -28,8 +28,8 @@ void triangulate(std::vector<Vertex> inVertices){
             if (v.z > minZ - 5000)
             minZ = v.z;
     }
-    int amountOfQuadsX = 10;
-    int amountOfQuadsZ = 10;
+    int amountOfQuadsX = 100;
+    int amountOfQuadsZ = 100;
 
     float quadLength = (maxX - minX)/amountOfQuadsX;
     float quadWidth = (maxZ - minZ)/amountOfQuadsZ;
@@ -53,56 +53,101 @@ void triangulate(std::vector<Vertex> inVertices){
         vPositionX = floor((v.x - minX)/quadLength);
         }
         //If the position is maximum or greater, it is forced to maximum
-        if (v.z >= maxZ - 1){
+        if (v.z >= maxZ - (10/amountOfQuadsZ)){
             vPositionZ = (amountOfQuadsZ-1);
         }
         else{
             vPositionZ = floor((v.z - minZ)/quadWidth);
         }
 
-        if (vPositionX >= 0 && vPositionX <= 9 && vPositionZ >= 0 && vPositionZ <= 9){
+        if (vPositionX >= 0 && vPositionX <= amountOfQuadsX-1 && vPositionZ >= 0 && vPositionZ <= amountOfQuadsZ-1){
         //temporarily using the color of the vertex to count how many points are at each quad
         quads.at(vPositionZ * amountOfQuadsX + vPositionX).r++;
         quads.at(vPositionZ * amountOfQuadsX + vPositionX).y = (quads.at(vPositionZ * amountOfQuadsX + vPositionX).y + v.y);
         }
     }
-    //filling in incomplete information
-    for (int i = 0; i< quads.size(); i++)
-    {
-        if (quads.at(i).r == 0){
-            float neighborSum = 0.f;
-            int sums = 0;
-            if (i > 0){
-                neighborSum += quads.at(i - 1).y;
-                sums++;
-            }
-            if (i >= amountOfQuadsX){
-                neighborSum += quads.at(i - amountOfQuadsX).y;
-                sums++;
-            }
-            if (i < quads.size() - 1){
-                neighborSum += quads.at(i + 1).y;
-                sums++;
-            }
-            if (i < quads.size() - amountOfQuadsX){
-                neighborSum += quads.at(i + amountOfQuadsX).y;
-                sums++;
-            }
-            //Finding the average of the neighbor-sums
-            if (sums > 0){
-                quads.at(i).y = neighborSum / sums;
-            }
-            else{
-                quads.at(i).y = 1;
-            }
+    //dividing to find average height
+    for (int i = 0; i< quads.size(); i++){
+        if (quads.at(i).r != 0){
+            quads.at(i).y = quads.at(i).y / quads.at(i).r;
         }
     }
-    for (Vertex q : quads){
-        if (q.r != 0){
-        q.y = q.y / q.r;
-        q.r = 0.f;
-        std::cout << q.x << " " << q.y << " " << q.z << "\n";
+    std::vector<int> approximationList;
+    int loopItr = 10;
+
+    for (int h = 0; h < loopItr; h++){
+
+        for (int i = 0; i< quads.size(); i++)
+            if (quads.at(i).r == 0)
+                approximationList.push_back(i);
+
+        //filling in incomplete information
+        for (int i = 0; i< approximationList.size(); i++)
+        {
+            float neighborSum = 0.f;
+            int sums = 0;
+
+            if (approximationList.at(i) > 1 && quads.at(approximationList.at(i) - 1).r > 0){
+                neighborSum += quads.at(approximationList.at(i) - 1).y;
+                sums++;
+            }
+            if (approximationList.at(i) > amountOfQuadsX && quads.at(approximationList.at(i) - amountOfQuadsX).r > 0){
+                neighborSum += quads.at(approximationList.at(i) - amountOfQuadsX).y;
+                sums++;
+            }
+            if (approximationList.at(i) < quads.size() - 1 && quads.at(approximationList.at(i) + 1).r > 0){
+                neighborSum += quads.at(approximationList.at(i) + 1).y;
+                sums++;
+            }
+            if (approximationList.at(i) < quads.size() - amountOfQuadsX && quads.at(approximationList.at(i) + amountOfQuadsX).r > 0){
+                neighborSum += quads.at(approximationList.at(i) + amountOfQuadsX).y;
+                sums++;
+            }
+
+            if (sums > 2)
+                quads.at(approximationList.at(i)).r = 1;
+            if (sums > 0)
+                quads.at(approximationList.at(i)).y = neighborSum/sums;
         }
+        approximationList.clear();
+        //On final iteration
+        if (h + 1 == loopItr)
+            //refill approximationlist
+
+            for(int j = 0; j< quads.size(); j++){
+                //Approximate one last time without quality assurance
+                if (quads.at(j).r == 0){
+                    float neighborSum = 0.f;
+                    int sums = 0;
+
+                    if (j > 1 ){
+                        neighborSum += quads.at(j - 1).y;
+                        sums++;
+                    }
+                    if (j > amountOfQuadsX ){
+                        neighborSum += quads.at(j - amountOfQuadsX).y;
+                        sums++;
+                    }
+                    if (j < quads.size() - 1 ){
+                        neighborSum += quads.at(j + 1).y;
+                        sums++;
+                    }
+                    if (j < quads.size() - amountOfQuadsX ){
+                        neighborSum += quads.at(j + amountOfQuadsX).y;
+                        sums++;
+                    }
+                    if (sums > 0)
+                        quads.at(j).y = neighborSum/sums;
+                    else{
+                        quads.at(j).y = 1;
+                    }
+                }
+            }
+    }
+
+    //removing color used for categorizing during approximation
+    for (int i = 0; i< quads.size(); i++){
+        quads.at(i).r = 0;
     }
 
     std::vector<Vertex> vertices;
@@ -110,29 +155,12 @@ void triangulate(std::vector<Vertex> inVertices){
     for (int i = 0; i < amountOfQuadsZ; i++){
         for (int j = 0; j < amountOfQuadsX; j++){
             vertices.push_back(Vertex(j * quadLength, quads.at(i * amountOfQuadsX + j).y, i * quadWidth, 0, 1, 0, 0, 0));
-            //std::cout << vertices.back() << "\n";
+            std::cout << vertices.back() << "\n";
         }
     }
 
-    std::vector<int> indices;
 
-    for (int i = 0; i < amountOfQuadsZ - 1; i++){
-        for (int j = 0; j < amountOfQuadsX - 1; j++){
-            //1st triangle
-            indices.push_back(i * amountOfQuadsX + j);
-            indices.push_back((i + 1) * amountOfQuadsX + j);
-            indices.push_back(i * amountOfQuadsX + j + 1);
-            //2nd triangle
-            indices.push_back(i * amountOfQuadsX + j + 1);
-            indices.push_back((i + 1) * amountOfQuadsX + j);
-            indices.push_back((i + 1) * amountOfQuadsX + j + 1);
-
-            //std::cout << indices.at(i * 6 * (amountOfQuadsX - 1) + (j * 6)) << " " << indices.at(i * 6 *(amountOfQuadsX - 1) + (j * 6) + 1) << " " << indices.at(i * 6 *(amountOfQuadsX - 1) + (j * 6) + 2) << "\n";
-            //std::cout << indices.at(i * 6 * (amountOfQuadsX - 1) + (j * 6) + 3) << " " << indices.at(i * 6 *(amountOfQuadsX - 1) + (j * 6) + 4) << " " << indices.at(i * 6 *(amountOfQuadsX - 1) + (j * 6) + 5) << "\n";
-        }
-    }
-
-    return;
+    return vertices;
 
 };
 #endif // TRIANGULERING_H

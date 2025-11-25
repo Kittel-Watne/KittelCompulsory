94e9a21657b4b1c57eb2827d59856db3e46efce4
Kittel <142875697+Kittel-Watne@users.noreply.github.com>
2025-11-25 10:46:54 +0100

    Finished vertexNormals

--- TriangleSurface.cpp
+++ TriangleSurface.cpp
@@ -2,6 +2,7 @@
 #include "Triangulering.h"
 #include <fstream>
 #include <QDebug>
+#include <iomanip>
 TriangleSurface::TriangleSurface() : VisualObject()
 {
     Vertex v1{ 0.0f,  0.0f,  0.0f,   1.0f, 0.0f, 0.0f,  0.0f, 0.0f};  //  bottom-left corner
@@ -74,77 +75,304 @@ TriangleSurface::TriangleSurface(const std::string &filename)
 
     //calculating normal vector for vertices
     std::vector<QVector3D> vertexNormals;
+    //normal vertex, top border, bottom border, left border, right border, top left, top right, bottom left, bottom right
+    std::array<int, 9> vertexResults {0, 0, 0, 0, 0, 0, 0, 0, 0};
     for (int i = 0; i < mVertices.size(); i++)
     {
-        QVector3D vertexNormal;
+        QVector3D vertexNormal{0, 0, 0};
+
         //Is it on the top border
         if (i < amountOfQuadsX){
             //Is it on the left corner
             if (i % amountOfQuadsX == 0 ){
+                //there is 1 triangle using it
 
+                //Triangle 1, bottom right
+                QVector3D vector1 = QVector3D(
+                    (mVertices.at(i+amountOfQuadsX).x - mVertices.at(i).x),
+                    (mVertices.at(i+amountOfQuadsX).y - mVertices.at(i).y),
+                    (mVertices.at(i+amountOfQuadsX).z - mVertices.at(i).z)
+                    );
+                QVector3D vector2 = QVector3D(
+                    (mVertices.at(i+1).x - mVertices.at(i).x),
+                    (mVertices.at(i+1).y - mVertices.at(i).y),
+                    (mVertices.at(i+1).z - mVertices.at(i).z)
+                    );
+                vertexNormal += QVector3D::crossProduct(vector1, vector2);
+                vertexResults.at(5)++;
             }
              //Is it on the right corner
             else if(i % amountOfQuadsX == (amountOfQuadsX - 1)){
+                //there is 1 triangle using it
+
+                //Triangle 1, bottom left
+                QVector3D vector1 = QVector3D(
+                    (mVertices.at(i+amountOfQuadsX).x - mVertices.at(i-1).x),
+                    (mVertices.at(i+amountOfQuadsX).y - mVertices.at(i-1).y),
+                    (mVertices.at(i+amountOfQuadsX).z - mVertices.at(i-1).z)
+                    );
+                QVector3D vector2 = QVector3D(
+                    (mVertices.at(i).x - mVertices.at(i-1).x),
+                    (mVertices.at(i).y - mVertices.at(i-1).y),
+                    (mVertices.at(i).z - mVertices.at(i-1).z)
+                    );
+                vertexNormal += QVector3D::crossProduct(vector1, vector2);
+                vertexResults.at(6)++;
 
             }
             //Is a normal border
             else{
+                //there are 2 triangles using it
 
+                //Triangle 1, bottom left
+                QVector3D vector1 = QVector3D(
+                    (mVertices.at(i+amountOfQuadsX).x - mVertices.at(i-1).x),
+                    (mVertices.at(i+amountOfQuadsX).y - mVertices.at(i-1).y),
+                    (mVertices.at(i+amountOfQuadsX).z - mVertices.at(i-1).z)
+                    );
+                QVector3D vector2 = QVector3D(
+                    (mVertices.at(i).x - mVertices.at(i-1).x),
+                    (mVertices.at(i).y - mVertices.at(i-1).y),
+                    (mVertices.at(i).z - mVertices.at(i-1).z)
+                    );
+                vertexNormal += QVector3D::crossProduct(vector1, vector2);
+
+                //Triangle 2, bottom right
+                vector1 = QVector3D(
+                    (mVertices.at(i+amountOfQuadsX).x - mVertices.at(i).x),
+                    (mVertices.at(i+amountOfQuadsX).y - mVertices.at(i).y),
+                    (mVertices.at(i+amountOfQuadsX).z - mVertices.at(i).z)
+                    );
+                vector2 = QVector3D(
+                    (mVertices.at(i+1).x - mVertices.at(i).x),
+                    (mVertices.at(i+1).y - mVertices.at(i).y),
+                    (mVertices.at(i+1).z - mVertices.at(i).z)
+                    );
+                vertexNormal += QVector3D::crossProduct(vector1, vector2);
+                vertexResults.at(1)++;
             }
         }
         //Is it on the bottom border
         else if(i >= mVertices.size() - amountOfQuadsX){
             //Is it on the left corner
             if (i % amountOfQuadsX == 0 ){
+                //there is 1 triangle using it
+
+                //Triangle 1, top right
+                QVector3D vector1 = QVector3D(
+                    (mVertices.at(i).x - mVertices.at(i-amountOfQuadsX).x),
+                    (mVertices.at(i).y - mVertices.at(i-amountOfQuadsX).y),
+                    (mVertices.at(i).z - mVertices.at(i-amountOfQuadsX).z)
+                    );
+                QVector3D vector2 = QVector3D(
+                    (mVertices.at(i+1).x - mVertices.at(i-amountOfQuadsX).x),
+                    (mVertices.at(i+1).y - mVertices.at(i-amountOfQuadsX).y),
+                    (mVertices.at(i+1).z - mVertices.at(i-amountOfQuadsX).z)
+                    );
+                vertexNormal += QVector3D::crossProduct(vector1, vector2);
+                vertexResults.at(7)++;
 
             }
             //Is it on the right corner
             else if(i % amountOfQuadsX == (amountOfQuadsX - 1)){
+                //there is 1 triangle using it
+
+                //Triangle 1, top left
+                QVector3D vector1 = QVector3D(
+                    (mVertices.at(i-1).x - mVertices.at(i-amountOfQuadsX).x),
+                    (mVertices.at(i-1).y - mVertices.at(i-amountOfQuadsX).y),
+                    (mVertices.at(i-1).z - mVertices.at(i-amountOfQuadsX).z)
+                    );
+                QVector3D vector2 = QVector3D(
+                    (mVertices.at(i).x - mVertices.at(i-amountOfQuadsX).x),
+                    (mVertices.at(i).y - mVertices.at(i-amountOfQuadsX).y),
+                    (mVertices.at(i).z - mVertices.at(i-amountOfQuadsX).z)
+                    );
+                vertexNormal += QVector3D::crossProduct(vector1, vector2);
+                vertexResults.at(8)++;
 
             }
             //Is a normal border
             else{
+                //there are 2 triangles using it
+                //Triangle 1, top left
+                QVector3D vector1 = QVector3D(
+                    (mVertices.at(i-1).x - mVertices.at(i-amountOfQuadsX).x),
+                    (mVertices.at(i-1).y - mVertices.at(i-amountOfQuadsX).y),
+                    (mVertices.at(i-1).z - mVertices.at(i-amountOfQuadsX).z)
+                    );
+                QVector3D vector2 = QVector3D(
+                    (mVertices.at(i).x - mVertices.at(i-amountOfQuadsX).x),
+                    (mVertices.at(i).y - mVertices.at(i-amountOfQuadsX).y),
+                    (mVertices.at(i).z - mVertices.at(i-amountOfQuadsX).z)
+                    );
+                vertexNormal += QVector3D::crossProduct(vector1, vector2);
 
+                //Triangle 2, top right
+                vector1 = QVector3D(
+                    (mVertices.at(i).x - mVertices.at(i-amountOfQuadsX).x),
+                    (mVertices.at(i).y - mVertices.at(i-amountOfQuadsX).y),
+                    (mVertices.at(i).z - mVertices.at(i-amountOfQuadsX).z)
+                    );
+                vector2 = QVector3D(
+                    (mVertices.at(i+1).x - mVertices.at(i-amountOfQuadsX).x),
+                    (mVertices.at(i+1).y - mVertices.at(i-amountOfQuadsX).y),
+                    (mVertices.at(i+1).z - mVertices.at(i-amountOfQuadsX).z)
+                    );
+                vertexNormal += QVector3D::crossProduct(vector1, vector2);
+                vertexResults.at(2)++;
             }
         }
         //Is it on the left border
-        if (i % amountOfQuadsX == 0 ){
+        else if (i % amountOfQuadsX == 0 ){
+            //there are 2 triangles using it
+
+
+            //Triangle 1, top right
+            QVector3D vector1 = QVector3D(
+                (mVertices.at(i).x - mVertices.at(i-amountOfQuadsX).x),
+                (mVertices.at(i).y - mVertices.at(i-amountOfQuadsX).y),
+                (mVertices.at(i).z - mVertices.at(i-amountOfQuadsX).z)
+                );
+            QVector3D vector2 = QVector3D(
+                (mVertices.at(i+1).x - mVertices.at(i-amountOfQuadsX).x),
+                (mVertices.at(i+1).y - mVertices.at(i-amountOfQuadsX).y),
+                (mVertices.at(i+1).z - mVertices.at(i-amountOfQuadsX).z)
+                );
+            vertexNormal += QVector3D::crossProduct(vector1, vector2);
+
+            //Triangle 2, bottom right
+            vector1 = QVector3D(
+                (mVertices.at(i+amountOfQuadsX).x - mVertices.at(i).x),
+                (mVertices.at(i+amountOfQuadsX).y - mVertices.at(i).y),
+                (mVertices.at(i+amountOfQuadsX).z - mVertices.at(i).z)
+                );
+            vector2 = QVector3D(
+                (mVertices.at(i+1).x - mVertices.at(i).x),
+                (mVertices.at(i+1).y - mVertices.at(i).y),
+                (mVertices.at(i+1).z - mVertices.at(i).z)
+                );
+            vertexNormal += QVector3D::crossProduct(vector1, vector2);
+            vertexResults.at(3)++;
 
         }
         //Is it on the right border
         else if(i % amountOfQuadsX == (amountOfQuadsX - 1)){
+            //there are 2 triangles using it
+
+            //Triangle 1, top left
+            QVector3D vector1 = QVector3D(
+                (mVertices.at(i-1).x - mVertices.at(i-amountOfQuadsX).x),
+                (mVertices.at(i-1).y - mVertices.at(i-amountOfQuadsX).y),
+                (mVertices.at(i-1).z - mVertices.at(i-amountOfQuadsX).z)
+                );
+            QVector3D vector2 = QVector3D(
+                (mVertices.at(i).x - mVertices.at(i-amountOfQuadsX).x),
+                (mVertices.at(i).y - mVertices.at(i-amountOfQuadsX).y),
+                (mVertices.at(i).z - mVertices.at(i-amountOfQuadsX).z)
+                );
+            vertexNormal += QVector3D::crossProduct(vector1, vector2);
+
+            //Triangle 2, bottom left
+            vector1 = QVector3D(
+                (mVertices.at(i+amountOfQuadsX).x - mVertices.at(i-1).x),
+                (mVertices.at(i+amountOfQuadsX).y - mVertices.at(i-1).y),
+                (mVertices.at(i+amountOfQuadsX).z - mVertices.at(i-1).z)
+                );
+            vector2 = QVector3D(
+                (mVertices.at(i).x - mVertices.at(i-1).x),
+                (mVertices.at(i).y - mVertices.at(i-1).y),
+                (mVertices.at(i).z - mVertices.at(i-1).z)
+                );
+            vertexNormal += QVector3D::crossProduct(vector1, vector2);
+            vertexResults.at(4)++;
 
         }
         //Is a normal vertex
         else{
             //there are 4 triangles using it
-            //Triangle 1
+            //Triangle 1, top left
             QVector3D vector1 = QVector3D(
-                (mVertices.at(i-amountOfQuadsX).x - mVertices.at(i).x),
-                (mVertices.at(i-amountOfQuadsX).y - mVertices.at(i).y),
-                (mVertices.at(i-amountOfQuadsX).z - mVertices.at(i).z)
+                (mVertices.at(i-1).x - mVertices.at(i-amountOfQuadsX).x),
+                (mVertices.at(i-1).y - mVertices.at(i-amountOfQuadsX).y),
+                (mVertices.at(i-1).z - mVertices.at(i-amountOfQuadsX).z)
                 );
             QVector3D vector2 = QVector3D(
-                (mVertices.at(i-amountOfQuadsX).x - mVertices.at(i).x),
-                (mVertices.at(i-amountOfQuadsX).y - mVertices.at(i).y),
-                (mVertices.at(i-amountOfQuadsX).z - mVertices.at(i).z)
+                (mVertices.at(i).x - mVertices.at(i-amountOfQuadsX).x),
+                (mVertices.at(i).y - mVertices.at(i-amountOfQuadsX).y),
+                (mVertices.at(i).z - mVertices.at(i-amountOfQuadsX).z)
                 );
-            QVector3D::crossProduct(vector1, vector2)
+            vertexNormal += QVector3D::crossProduct(vector1, vector2);
+
+            //Triangle 2, top right
+            vector1 = QVector3D(
+                (mVertices.at(i).x - mVertices.at(i-amountOfQuadsX).x),
+                (mVertices.at(i).y - mVertices.at(i-amountOfQuadsX).y),
+                (mVertices.at(i).z - mVertices.at(i-amountOfQuadsX).z)
+                );
+            vector2 = QVector3D(
+                (mVertices.at(i+1).x - mVertices.at(i-amountOfQuadsX).x),
+                (mVertices.at(i+1).y - mVertices.at(i-amountOfQuadsX).y),
+                (mVertices.at(i+1).z - mVertices.at(i-amountOfQuadsX).z)
+                );
+            vertexNormal += QVector3D::crossProduct(vector1, vector2);
+
+            //Triangle 3, bottom left
+            vector1 = QVector3D(
+                (mVertices.at(i+amountOfQuadsX).x - mVertices.at(i-1).x),
+                (mVertices.at(i+amountOfQuadsX).y - mVertices.at(i-1).y),
+                (mVertices.at(i+amountOfQuadsX).z - mVertices.at(i-1).z)
+                );
+            vector2 = QVector3D(
+                (mVertices.at(i).x - mVertices.at(i-1).x),
+                (mVertices.at(i).y - mVertices.at(i-1).y),
+                (mVertices.at(i).z - mVertices.at(i-1).z)
+                );
+            vertexNormal += QVector3D::crossProduct(vector1, vector2);
+
+            //Triangle 4, bottom right
+            vector1 = QVector3D(
+                (mVertices.at(i+amountOfQuadsX).x - mVertices.at(i).x),
+                (mVertices.at(i+amountOfQuadsX).y - mVertices.at(i).y),
+                (mVertices.at(i+amountOfQuadsX).z - mVertices.at(i).z)
+                );
+            vector2 = QVector3D(
+                (mVertices.at(i+1).x - mVertices.at(i).x),
+                (mVertices.at(i+1).y - mVertices.at(i).y),
+                (mVertices.at(i+1).z - mVertices.at(i).z)
+                );
+            vertexNormal += QVector3D::crossProduct(vector1, vector2);
+            vertexResults.at(0)++;
         }
+        vertexNormal.normalize();
+
+        vertexNormals.push_back(vertexNormal);
     }
-    //Printing to .obj
+    //error testing
     /*
+    for (int i : vertexResults){
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+
+    for (QVector3D n : vertexNormals)
+        //std::cout << abs(n.x()) << " " << abs(n.y()) << " " << abs(n.z()) << "\n";
+        std::cout << n.x() << " " << n.y() << " " << n.z() << "\n";
+    */
+
+    //Printing to .obj
     std::ofstream out("lasData.obj");
     if (!out.is_open())
         return;
-
     for (Vertex v : mVertices){
-        out << "v " << v.x << " " << v.y << " " << v.z << "\n";
+        out << "v " << std::fixed << std::setprecision(4) << v.x << " " << v.y << " " << v.z << "\n";
+    }
+    for (QVector3D n : vertexNormals){
+        out << "vn " << std::fixed << std::setprecision(4) << n.x() << " " << n.y() << " " << n.z() << "\n";
     }
     for (int i = 0; i < mIndices.size(); i += 3){
         //Obj uses base 1 indices
-        out << "f " << (mIndices.at(i) + 1) << " " << (mIndices.at(i + 1) + 2) << " " << (mIndices.at(i + 2) + 3) << "\n";
+        out << "f " << (mIndices.at(i) + 1) << " " << (mIndices.at(i + 1) + 1) << " " << (mIndices.at(i + 2) + 1) << "\n";
     }
     out.close();
-*/
 }
